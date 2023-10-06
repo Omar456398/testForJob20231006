c601e5759ca2a1b5959adbcdc93cd9326299382c
Omar Raafat Enany <58509199+Omar456398@users.noreply.github.com>
2023-10-06 10:53:41 +0300

    Update solution.js

--- solution.js
+++ solution.js
@@ -1,1 +1,12 @@
-
+function returnLongestWord(stringOfWords) {
+  const arrayOfWords = stringOfWords.toLowerCase().split('').filter(item =>/[a-z ]/.test(item)).join('').split(' ').filter(item => item.length)
+  let longestWord = ''
+  arrayOfWords.forEach(item => {
+    if(item.length > longestWord.length) {
+      longestWord = item
+    } else if (item.length === longestWord.length && item.replace(/[aeiou]/g, '##') > longestWord.replace(/[aeiou]/g, '##')) {
+      longestWord = item
+    }
+  })
+  return longestWord
+}
